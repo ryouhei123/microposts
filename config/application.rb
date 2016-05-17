126f05dff05dd4a459e58726e69a63d166d96bea
ryouhei123 <buenasuerte25@gmail.com>
2016-05-17 22:20:33 +0000

    add initial setups

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,13 @@ Bundler.require(*Rails.groups)
 
 module Microposts
   class Application < Rails::Application
+    
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -21,6 +28,5 @@ module Microposts
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
   end
 end
