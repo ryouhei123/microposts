ff862cfcd0fdb13ad8b45e3600da0c1063844e7f
ryouhei123 <buenasuerte25@gmail.com>
2016-05-23 10:37:45 +0000

    user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+  before_action: logged_in_user, only:[:edit, :update]
   def show 
    @user = User.find(params[:id])
   end
@@ -17,11 +17,36 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+    if (current_user != @user)
+      redirect_to: root
+    end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if (current_user != @user)
+      redirect_to: root
+    end
+    if (@user.update(user_profile))
+      flash[:success] = "更新しました。"
+      redirect_to @user 
+    end
+  end
+    
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+  
+  def user_profile
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation,
+                                 :profile)
   end
 end
