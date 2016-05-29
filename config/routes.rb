b978c888d70154b2033c7335ce98b07a8f655f55
ryouhei123 <buenasuerte25@gmail.com>
2016-05-29 11:03:03 +0000

    totyuu

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
   get 'signup',  to: 'users#new'
 
-  resources :users
+  resources :users do
+    member do
+      get 'followings'
+      get 'followers'
+    end
+  end
   resources :microposts
   resources :relationships, only: [:create, :destroy]
 
